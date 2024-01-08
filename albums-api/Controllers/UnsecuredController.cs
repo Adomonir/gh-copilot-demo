b116ba481d817ec79a4b129d86c21f206ec1f7a2
Adeola <Adeola.Omonira@microsoft.com>
2024-01-08 16:54:18 -0500

    Update user authentication logic

--- albums-api/Controllers/UnsecuredController.cs
+++ albums-api/Controllers/UnsecuredController.cs
@@ -1,58 +1,69 @@
-using Microsoft.Data.SqlClient;
-using System.Data;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Text;
+using System.IO;
 using System.Text;
 
 namespace UnsecureApp.Controllers
 {
+    /// <summary>
+    /// Controller for handling file operations.
+    /// </summary>
     public class MyController
     {
-
+        /// <summary>
+        /// Reads the content of a file.
+        /// </summary>
+        /// <param name="userInput">The relative path of the file to read.</param>
+        /// <returns>The content of the file as a string.</returns>
+        /// <exception cref="UnauthorizedAccessException">Thrown when an attempt is made to access a file outside of the current directory.</exception>
         public string ReadFile(string userInput)
         {
-            using (FileStream fs = File.Open(userInput, FileMode.Open))
+            string safePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), userInput));
+
+            if (!safePath.StartsWith(Directory.GetCurrentDirectory()))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            StringBuilder fileContent = new StringBuilder();
+
+            using (FileStream fs = File.Open(safePath, FileMode.Open))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                // Rest of the code...
+            }
+        }
+    }
+}
+namespace UnsecureApp.Controllers
+{
+    public class MyController
+    {
+        public string ReadFile(string userInput)
+        {
+            string safePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), userInput));
+
+            if (!safePath.StartsWith(Directory.GetCurrentDirectory()))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            StringBuilder fileContent = new StringBuilder();
+
+            using (FileStream fs = File.Open(safePath, FileMode.Open))
             {
                 byte[] b = new byte[1024];
                 UTF8Encoding temp = new UTF8Encoding(true);
 
-                while (fs.Read(b, 0, b.Length) > 0)
+                int bytesRead;
+                while ((bytesRead = fs.Read(b, 0, b.Length)) > 0)
                 {
-                    return temp.GetString(b);
+                    fileContent.Append(temp.GetString(b, 0, bytesRead));
                 }
             }
 
-            return null;
+            return fileContent.ToString();
         }
-
-        public int GetProduct(string productName)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand sqlCommand = new SqlCommand()
-                {
-                    CommandText = "SELECT ProductId FROM Products WHERE ProductName = '" + productName + "'",
-                    CommandType = CommandType.Text,
-                };
-
-                SqlDataReader reader = sqlCommand.ExecuteReader();
-                return reader.GetInt32(0); 
-            }
-        }
-
-        public void GetObject()
-        {
-            try
-            {
-                object o = null;
-                o.ToString();
-            }
-            catch (Exception e)
-            {
-                this.Response.Write(e.ToString());
-            }
-        
-        }
-
-        private string connectionString = "";
     }
 }
